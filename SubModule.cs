232f25426d60583ed02aff69aec5f987e7f38bb3
Daniel Albright <albri118@morris.umn.edu>
2022-04-22 17:47:20 -0500

    Create new health increase item
    Following Lesser Scholar's (https://www.youtube.com/channel/UCstX1fHnzHF2QrneU7c1Tmw) Artisan Workshop Mod Tutorial.
    
    So far I have added an artisan beer item that, when Q is pressed in a mission view, will heal the player character by 20 points (up to max, decrement beer amount on q press).

--- SubModule.cs
+++ SubModule.cs
@@ -1,4 +1,9 @@
-﻿using TaleWorlds.MountAndBlade;
+﻿using System;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.MountAndBlade.View.Missions;
+using TaleWorlds.ObjectSystem;
 
 namespace CookieLord
 {
@@ -10,16 +15,40 @@ namespace CookieLord
 
         }
 
-        protected override void OnSubModuleUnloaded()
+        public override void OnMissionBehaviorInitialize(Mission mission)
         {
-            base.OnSubModuleUnloaded();
+            base.OnMissionBehaviorInitialize(mission);
+            mission.AddMissionBehavior(new ArtisanBeerMissionView());
+        }
+    }
 
+    public class ArtisanBeerMissionView : MissionView 
+    {
+        public override void OnMissionScreenTick(float dt)
+        {
+            base.OnMissionScreenTick(dt);
+
+            if (Input.IsKeyPressed(TaleWorlds.InputSystem.InputKey.Q)) 
+            {
+                DrinkBeer();
+            }
         }
 
-        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        private void DrinkBeer()
         {
-            base.OnBeforeInitialModuleScreenSetAsRoot();
+            // Check for artisan beer in inventory
+            var itemRoster = MobileParty.MainParty.ItemRoster;
+            var artisanBeerObject = MBObjectManager.Instance.GetObject<ItemObject>("artisan_beer");
+            if (itemRoster.GetItemNumber(artisanBeerObject) <= 0) return;
 
+            // Remove one artisan beer
+            itemRoster.AddToCounts(artisanBeerObject, -1);
+
+            // Increase player hp
+            Mission.MainAgent.Health += 20;
+            InformationManager.DisplayMessage(new InformationMessage(
+                String.Format("Health increased by {0}, now at {1}", 20, Mission.MainAgent.Health)
+                ));
         }
     }
 }
