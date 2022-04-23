ae45854595a66bf6b38aa24683ec4773648e1560
Daniel Albright <albri118@morris.umn.edu>
2022-04-23 10:35:00 -0500

    Add artisan beer behavior
    currently subtracts gold from brewery workshops daily income (artisan beer expense)

--- SubModule.cs
+++ SubModule.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.CampaignSystem;
 using TaleWorlds.Core;
 using TaleWorlds.MountAndBlade;
-using TaleWorlds.MountAndBlade.View.Missions;
-using TaleWorlds.ObjectSystem;
 
 namespace CookieLord
 {
@@ -20,64 +18,13 @@ namespace CookieLord
             base.OnMissionBehaviorInitialize(mission);
             mission.AddMissionBehavior(new ArtisanBeerMissionView());
         }
-    }
 
-    public class ArtisanBeerMissionView : MissionView 
-    {
-        public override void OnMissionScreenTick(float dt)
+       protected override void InitializeGameStarter(Game game, IGameStarter starterObject)
         {
-            base.OnMissionScreenTick(dt);
-
-            if (Input.IsKeyPressed(TaleWorlds.InputSystem.InputKey.Q)) 
+            if (starterObject is CampaignGameStarter starter)
             {
-                if (Mission.Mode is MissionMode.Battle or MissionMode.Stealth or MissionMode.Duel or MissionMode.Tournament)
-                {
-                    DrinkBeer();
-                }
-                else return;
+                starter.AddBehavior(new ArtisanBeerBehavior());
             }
-        }
-
-        private void DrinkBeer()
-        {
-            // Check for artisan beer in inventory
-            var itemRoster = MobileParty.MainParty.ItemRoster;
-            var artisanBeerObject = MBObjectManager.Instance.GetObject<ItemObject>("artisan_beer");
-            var dm = new DisplayMessages();
-            var ma = Mission.MainAgent;
-
-            if (itemRoster.GetItemNumber(artisanBeerObject) <= 0) {
-                dm.ShowMessage(dm.CreateBasicInfoMessage("No Artisan Beer in Inventory"));
-                return;
-            }
-            
-            // Check for player health at limit
-            if (ma.Health >= ma.HealthLimit)
-            {
-                dm.ShowMessage(dm.CreateBasicInfoMessage("Health is full, cannot use Artisan Beer"));
-                return;
-            }
-
-            // Remove one artisan beer
-            itemRoster.AddToCounts(artisanBeerObject, -1);
-
-            // Check that player hp is not at the limit
-
-            // Increase player hp
-            var healthAdded = ma.HealthLimit - ma.Health;
-            if (healthAdded >= 20)
-            {
-                ma.Health += 20;
-                healthAdded = 20;
-            }
-            else
-            {
-                ma.Health += healthAdded;
-            }
-
-            // Display Health Message
-            var mesArgs = new object[] { healthAdded, ma.Health };
-            dm.ShowMessage(dm.CreateFormattedInfoMessage("Healed {0} hp. Currently at {1}hp", mesArgs));
         }
     }
 }
